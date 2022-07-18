10fdd2d46f1fffd9f32d0e29b4095267a4cc1795
betul.kilincaslan <betul.kilincaslan@inveon.com>
2022-07-18 22:05:50 +0300

    feat: removed form button's margin bottom

--- src/components/common/FormButton.js
+++ src/components/common/FormButton.js
@@ -1,7 +1,7 @@
 const FormButton = (props) => {
   return (
     <button
-      className={`w-full bg-${props.color}-500 hover:bg-${props.color}-400 text-white font-bold py-2 px-4 mb-6 rounded tracking-wide transition-all duration-300`}
+      className={`w-full bg-${props.color}-500 hover:bg-${props.color}-400 text-white font-bold py-2 px-4 rounded tracking-wide transition-all duration-300`}
       type={props.type || "button"}
       onClick={props.onClick}
     >
