7f0f48ae1d2e7d26837b18560f3bc2eea2454049
betul.kilincaslan <betul.kilincaslan@inveon.com>
2022-07-19 19:15:56 +0300

    feat: spinner component created

---
+++ src/components/common/Spinner.js
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center mt-20">
+      <div className="w-12 h-12 rounded-full animate-spin border-4 border-solid border-yellow-500 border-t-transparent shadow-md"></div>
+    </div>
+  );
+};
+
+export default Spinner;
