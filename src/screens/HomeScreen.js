a86ac4220f32d46e8c4b7e83713613850c44532f
betul.kilincaslan <betul.kilincaslan@inveon.com>
2022-07-19 19:42:09 +0300

    feat: todos moved to the parent component, spinner added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,29 @@
-import AddTodoModalContainer from "components/todo/AddTodoModalContainer";
+import AddTodoContainer from "components/todo/AddTodoContainer";
 import TodoTable from "components/todo/TodoTable";
+import { useSelector, useDispatch } from "react-redux";
+import { getTodosAsync, todoData } from "redux/todos/todosSlice";
+import Spinner from "components/common/Spinner";
+import { useEffect } from "react";
 
 const HomeScreen = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector(todoData);
+  const isLoading = useSelector((state) => state.todos.isLoading);
+
+  useEffect(() => {
+    dispatch(getTodosAsync());
+  }, [dispatch]);
+
   return (
     <>
-      <AddTodoModalContainer />
-      <TodoTable />
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <AddTodoContainer />
+          <TodoTable todos={todos} />
+        </>
+      )}
     </>
   );
 };
