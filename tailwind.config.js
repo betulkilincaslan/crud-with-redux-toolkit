deb62ca001adf1220dd655cdfdc38374edbaacb3
betul.kilincaslan <betul.kilincaslan@inveon.com>
2022-07-19 23:35:48 +0300

    feat: ui kit color added and bg color changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fuelTown: "#576574",
+        imperialPrimer: "#222f3e",
+        lightBlueBallerina: "#ecf0f1",
+        stormPetrel: "#8395a7",
+        pastelRed: "#ff6b6b",
+        amour: "#ee5253",
+        casandoraYellow: "#feca57",
+        doubleDragonSkin: "#ff9f43",
+        wildCaribbean: "#1dd1a1",
+        darkMountain: "#10ac84",
+      },
+    },
   },
   plugins: [],
 };
