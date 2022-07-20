d9b7b269c3e6bee12d3387bf9ddd6a110f714f71
betulkilincaslan <betulkilincaslan@gmail.com>
2022-07-20 19:06:48 +0300

    chore: changed file name and directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "styles/globals.css";
 import App from "./App";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter } from "react-router-dom";
