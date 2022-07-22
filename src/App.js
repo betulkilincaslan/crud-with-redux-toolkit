fa9365669fffad54723de473e346864348adef9a
betulkilincaslan <betulkilincaslan@gmail.com>
2022-07-22 15:51:03 +0300

    feat: login logout operations done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import ScrollToTopButton from "components/common/ScrollToTopButton";
 import ProtectedRoute from "components/routing/ProtectedRoute";
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import HomeScreen from "screens/HomeScreen";
 import LoginScreen from "screens/LoginScreen";
+import { useNavigate } from "react-router-dom";
 
 const App = () => {
+  const [isLoggedin, setIsLoggedin] = useState(false);
+
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      setIsLoggedin(true);
+      navigate("/todos");
+    }
+  }, [navigate]);
+
   return (
     <main className="container mx-auto px-4 py-12 max-w-6xl">
       <ScrollToTopButton />
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,15 +4,24 @@ import { getTodosAsync, todoData } from "redux/todos/todosSlice";
 import Spinner from "components/common/Spinner";
 import { useEffect } from "react";
 import TodosContainer from "components/todo/TodosContainer";
+import { useNavigate } from "react-router-dom";
 
 const HomeScreen = () => {
   const dispatch = useDispatch();
   const todos = useSelector(todoData);
   const isLoading = useSelector((state) => state.todos.isLoading);
 
+  const navigate = useNavigate();
+
   useEffect(() => {
     dispatch(getTodosAsync());
   }, [dispatch]);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    navigate("/");
+  };
 
   return (
     <>
@@ -22,6 +31,15 @@ const HomeScreen = () => {
         <>
           <AddTodoContainer />
           <TodosContainer todos={todos} />
+          <div className="flex cursor-pointer mt-8">
+            <span
+              className="font-semibold text-sm tracking-wide text-clouds hover:text-silver transition-all duration-200 flex items-center"
+              onClick={logoutHandler}
+            >
+              Logout
+              <i className="bx bx-log-out-circle"></i>
+            </span>
+          </div>
         </>
       )}
     </>
